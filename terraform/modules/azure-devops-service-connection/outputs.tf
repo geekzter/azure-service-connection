9e665ddc0a92891d46ff071d578de1548b1fd2c0
Eric van Wijk <eric@van-wijk.com>
2024-07-25 16:15:31 +0200

    Fix federated credential race condition

--- terraform/modules/azure-devops-service-connection/outputs.tf
+++ terraform/modules/azure-devops-service-connection/outputs.tf
@@ -6,9 +6,11 @@ output service_connection_id {
 }
 output service_connection_oidc_issuer {
   value       = azuredevops_serviceendpoint_azurerm.azurerm.workload_identity_federation_issuer
+  depends_on  = [time_sleep.fic_destroy_race_condition]
 }
 output service_connection_oidc_subject {
   value       = azuredevops_serviceendpoint_azurerm.azurerm.workload_identity_federation_subject
+  depends_on  = [time_sleep.fic_destroy_race_condition]
 }
 output service_connection_url {
   value       = "${data.azuredevops_client_config.current.organization_url}/${replace(var.project_name," ","%20")}/_settings/adminservices?resourceId=${azuredevops_serviceendpoint_azurerm.azurerm.id}"
